53ba72e2871d4b99a5e7eb619b75146adf4c9ef1
Joshua Bachmeier <uwdkl@student.kit.edu>
2017-02-28 15:29:47 +0100

    Automatically fill OP model based on well known configuration

--- openid/admin.py
+++ openid/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
+from requests.compat import urljoin
+
 from .models import Provider
 
-admin.site.register(Provider)
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+
+    fields = ('client_name', 'client_id', 'client_secret',
+              'provider_name', 'provider_uri', 'logo_uri')
+
+    def save_model(self, request, obj, form, change):
+        obj.from_wellknown_configuration()
+        obj.redirect_uri = urljoin(request.scheme + '://' + request.get_host(), 'openid')
+        super(ProviderAdmin, self).save_model(request, obj, form, change)
