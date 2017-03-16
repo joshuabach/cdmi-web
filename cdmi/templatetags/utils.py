336f038330ce2214b48a3945fef2ec5d1df5154b
Joshua Bachmeier <uwdkl@student.kit.edu>
2017-03-16 11:14:08 +0100

    Add iso duration display to dict.html

--- cdmi/templatetags/utils.py
+++ cdmi/templatetags/utils.py
@@ -4,6 +4,9 @@ import re
 from django import template
 from django.template.defaultfilters import stringfilter
 
+from isodate import parse_duration
+from isodate.isoerror import ISO8601Error
+
 
 register = template.Library()
 
@@ -49,3 +52,16 @@ def is_string(value):
 def replace(value, arg):
     args = arg.split(",")
     return value.replace(args[0], args[1])
+
+
+@register.filter
+@stringfilter
+def isoduration(value):
+    try:
+        duration = parse_duration(value)
+    except ISO8601Error:
+        return None
+    else:
+        duration = "{}".format(duration)
+        duration = duration.replace(", 0:00:00", "")
+        return duration
